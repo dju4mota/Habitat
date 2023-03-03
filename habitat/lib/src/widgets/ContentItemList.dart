0b056df3d9e543c6be20345e5d080b556f729952
dju4mota <fernando.b@ges.inatel>
2023-03-03 15:33:53 -0300

    Assests e desgin para widget de conteudo

--- habitat/lib/src/widgets/ContentItemList.dart
+++ habitat/lib/src/widgets/ContentItemList.dart
@@ -1,12 +1,18 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:habitat/src/models/Content.dart';
 import 'package:habitat/src/utils/utils.dart';
 
 class ContentItemList extends StatelessWidget {
-  Content content;
+  Content question;
+  List<Content> answers = [];
+
   Function openContent;
-  Function deleteContent;
-  ContentItemList(this.content, this.openContent, this.deleteContent);
+  // Function deleteContent;
+
+  ContentItemList(this.question, this.openContent, this.answers);
+  // , this.openContent, this.deleteContent);
 
   @override
   Widget build(BuildContext context) {
@@ -24,50 +30,64 @@ class ContentItemList extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.9,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20.0, 5, 5, 5),
-                    child: SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.55,
-                        child: Text(content.title,
-                            style: TextStyle(fontSize: 24, color: Color.fromARGB(255, 5, 54, 116)))),
-                  ),
-                  IconButton(
-                      onPressed: () {
-                        openContent(content);
-                      },
-                      icon: Icon(Icons.arrow_forward))
-                ],
-              ),
-            ),
+            ContentHeader(question),
             Padding(
               padding: const EdgeInsets.fromLTRB(20.0, 5, 5, 20.0),
               child: SizedBox(
                   width: MediaQuery.of(context).size.width * 0.9,
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.62,
-                        child: Text(
-                          content.description,
-                          style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 5, 54, 116)),
-                        ),
-                      ),
-                      IconButton(
-                          onPressed: () {
-                            deleteContent(content);
-                          },
-                          icon: Icon(Icons.delete))
-                    ],
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.62,
+                    child: Text(
+                      question.description,
+                      style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 5, 54, 116)),
+                    ),
                   )),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * answers.length * 0.13,
+              child: ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                padding: const EdgeInsets.all(0),
+                itemCount: answers.length,
+                itemBuilder: (context, index) {
+                  return ContentHeader(answers[index]);
+                },
+              ),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class ContentHeader extends StatelessWidget {
+  const ContentHeader(this.content);
+
+  final Content content;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(4, 0, 1, 4),
+          child: CircleAvatar(
+            radius: 32, // Image radius
+            backgroundImage: AssetImage("assets/mamonas-assassinas.jpg"),
+          ),
+        ),
+        Column(
+          children: [
+            SizedBox(
+                width: MediaQuery.of(context).size.width * 0.65,
+                child: Text(content.userId, style: TextStyle(fontSize: 24, color: Color.fromARGB(255, 5, 54, 116)))),
+            SizedBox(
+                width: MediaQuery.of(context).size.width * 0.65,
+                child: Text(content.title, style: TextStyle(fontSize: 24, color: Color.fromARGB(255, 5, 54, 116)))),
+          ],
+        ),
+      ],
     );
   }
 }
